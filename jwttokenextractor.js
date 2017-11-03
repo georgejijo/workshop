3b027f28d84a329436150c2cf12e200c34b2fabc
George <533907@cognizant.com>
2017-11-03 15:27:08 +0530

    added token verification changes

--- jwttokenextractor.js
+++ jwttokenextractor.js
@@ -1,19 +1,67 @@
+// grab the packages we need
+var nJwt = require('njwt');
+//var rijndael = require('node-rijndael');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
+var http = require('http');
+var signKey = 'secret';
+var encrypKey ='abcdefghijklmnopqrstuvwxyzabcdef'
 
-app.post("/", function(req, res){
-  if(req.query)
-  {
-    console.log(req.query.Analytics);
-  }
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// POST http://localhost:8080/
+// parameters sent with 
+app.post('/', function(req, res) {
+    var token=null;
+    if(req.query)
+    {
+      token = req.query.Analytics; 
+    }
+
+    if(token)
+    {
+        var jwtverified = nJwt.verify(token, signKey.toString(), 'RS256', function(err,decodedJwt){
+            if(err)
+            {
+              console.log('##### ERROR ######' + err); // Token has expired, has been tampered with, etc  
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              var html = '<!DOCTYPE html><html><head><title>JWT Token Details</title></head><body><div style="max-width: 600px;overflow-wrap: break-word;">';
+              html += '<h2>Token Error</h2></ br>';
+              html += err;
+              html += '</div></body></html>';
+              res.end(html, 'utf-8');            
+            }
+            else
+            {
+              //console.log(jwtverified.header["alg"]);
+              console.log(decodedJwt);              
+              // Will contain the header and body
+              //var plaintext = rijndael.decrypt(decodedJwt.ctxt, encrypKey, iv);
+              //req.decoded = decodedJwt;
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              var html = '<!DOCTYPE html><html><head><title>JWT Token Details</title></head><body><div style="max-width: 600px;overflow-wrap: break-word;">';
+              html += '<h2>Token Details</h2></ br>';
+              html += decodedJwt.ctxt;
+              html += '</div></body></html>';
+              res.end(html, 'utf-8');
+            }
+        });        
+    }
+    else
+    {
+        console.log('This is not valid token');
+    }
 }).on("error", function(e){
   console.log("Got error: " + e.message);
 });
 
 // Tell our app to listen on port 8080
-app.listen(process.env.PORT || 5000, function (err) {
+app.listen(port, function (err) {
   if (err) {
     throw err
   }
-console.log('Server started on port 8080')
-})
+  console.log('Server started! At http://localhost:' + port);
+});
