aa3feec33ae1c16d2ea85e75634fe534f540e98f
George <533907@cognizant.com>
2017-11-06 19:59:46 +0530

    expire checking

--- jwttokenextractor.js
+++ jwttokenextractor.js
@@ -1,19 +1,17 @@
 // grab the packages we need
 var nJwt = require('jws');
 
-//var rijndael = require('node-rijndael');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var http = require('http');
-var signKey = process.env.signKey;
+var signKey = process.env.signKey || 'secret';
 var encrypKey = process.env.encrypKey;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// POST http://localhost:8080/
 // parameters sent with 
 app.post('/', function(req, res) {
     var token=null;
@@ -25,17 +23,26 @@ app.post('/', function(req, res) {
     if(token)
     {
       try {
-        var jwtverified = nJwt.verify(token, 'HS256', signKey)
+        var jwtverified = nJwt.verify(token, 'HS256', signKey);
 
         if(jwtverified)
           {
-            var decodedJwt = nJwt.decode(token, {complete: true});
+            //Display details if verified
+            var decodedJwt = nJwt.decode(token, {complete: true}, function(err, decoded) {
+            if(err){
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              var html = '<!DOCTYPE html><html><head><title>JWT Token Details</title></head><body><div style="max-width: 600px;overflow-wrap: break-word;">';
+              html += '<h2>Token Error</h2></ br>';
+              html += err.message;
+              html += '</div></body></html>';
+              res.end(html, 'utf-8');
+            }else{
+              
+            }
+          });
             console.log(decodedJwt.header);
             console.log(decodedJwt.payload);
 
-            // Will contain the header and body
-            //var plaintext = rijndael.decrypt(decodedJwt.ctxt, encrypKey, iv);
-            //req.decoded = decodedJwt;
             res.writeHead(200, { 'Content-Type': 'text/html' });
             var html = '<!DOCTYPE html><html><head><title>JWT Token Details</title></head><body><div style="max-width: 600px;overflow-wrap: break-word;">';
             html += '<h2>Token Details</h2></ br>';
@@ -72,7 +79,6 @@ app.listen(port, function (err) {
   if (err) {
     throw err
   }
-
+  
   console.log('Server started! At http://localhost:' + port);
 });
- 
